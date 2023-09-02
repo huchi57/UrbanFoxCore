5671bc0333b8e64db2c0f17faf2e220d2ed94599
huchi57 <huchi.hello@gmail.com>
2023-09-02 23:46:52 +0900

    Feat: Added Localization, and raised version number to 2023.9.0

---
+++ Scripts/Editor/EditorWindows/LocalizationBrowser.cs
@@ -0,0 +1,398 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Networking;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace UrbanFox.Editor
+{
+    public class LocalizationBrowser : EditorWindow
+    {
+        //[SerializeField] private string m_fullURL = string.Empty;
+        //[SerializeField] private string m_searchText = string.Empty;
+
+        //[MenuItem("OwO/Window/Localization Browser...")]
+        //private static void Init()
+        //{
+        //    var window = GetWindow<LocalizationBrowser>();
+        //    window.titleContent = new GUIContent("Localization");
+        //    window.Show();
+        //}
+
+        //private void OnGUI()
+        //{
+        //    GUILayout.Label("Spreadsheet Settings", EditorStyles.boldLabel);
+        //    GUILayout.BeginHorizontal();
+        //    if (GUILayoutExtensions.ColoredButton("View Spreadsheet in Online Browser...", Color.yellow, GUILayout.Height(40)))
+        //    {
+        //    }
+        //    if (GUILayoutExtensions.ColoredButton("Update Latest from Online Spreadsheet...", Color.yellow, GUILayout.Height(40)))
+        //    {
+        //    }
+        //    GUILayout.EndHorizontal();
+        //    GUILayoutExtensions.HorizontalLine();
+
+        //    GUILayout.Label("Languages", EditorStyles.boldLabel);
+        //    GUILayout.BeginHorizontal();
+        //    GUILayout.EndHorizontal();
+        //    GUILayoutExtensions.HorizontalLine();
+
+        //    m_searchText = EditorGUILayoutExtensions.SearchText("Keyword", m_searchText);
+        //    GUILayoutExtensions.HorizontalLine();
+        //}
+        private const string _sourceSpreadsheetIDKey = "UrbanFox.Editor.LocalizationBrowser.SpreadsheetSourceKey.";
+        private const string _assetCreationFolder = "Assets/Resources/" + Localization.LanguageXMLAssetResourceFolder;
+
+        private const string _IDToken = "{ID}";
+        private const string _sourceSpreadsheetURLOriginalFormat = "https://docs.google.com/spreadsheets/d/" + _IDToken;
+        private const string _sourceSpreadsheetURLJsonFormat = "https://docs.google.com/spreadsheets/d/" + _IDToken + "/gviz/tq";
+
+        private static Vector2 _scroll = default;
+
+        [SerializeField] private string _sourceSpreadsheetID = string.Empty;
+        [SerializeField] private string _searchKey = string.Empty;
+
+        private UnityWebRequest _webRequest = null;
+        private string _downloadedJSONText = string.Empty;
+        private List<List<string>> _languageAndKeyData = null;
+        private bool[] _visibleLanguages = null;
+
+        private bool IsDownloading => _webRequest != null && _webRequest.result == UnityWebRequest.Result.InProgress;
+
+        private bool[] VisibleLanguages
+        {
+            get
+            {
+                if (_visibleLanguages == null && Localization.IsInitialized)
+                {
+                    _visibleLanguages = new bool[Localization.NumberOfLanguages];
+                    for (int i = 0; i < _visibleLanguages.Length; i++)
+                    {
+                        _visibleLanguages[i] = true;
+                    }
+                }
+                return _visibleLanguages;
+            }
+        }
+
+        private int VisibleLanguageCountPlusOne => VisibleLanguages.IsNullOrEmpty() ? 1 : VisibleLanguages.Where(x => x == true).ToList().Count + 1;
+        private float InspectorWidth => EditorGUIUtility.currentViewWidth - 30;
+
+        [MenuItem("OwO/Window/Localization Browser...")]
+        private static void Init()
+        {
+            var window = GetWindow<LocalizationBrowser>();
+            window.titleContent = new GUIContent("Localization Browser");
+            window.minSize = new Vector2(500, 500);
+            window.Show();
+        }
+
+        private void OnEnable()
+        {
+            _sourceSpreadsheetID = EditorPrefs.GetString(_sourceSpreadsheetIDKey + Application.productName, string.Empty);
+        }
+
+        private void OnDisable()
+        {
+            _webRequest?.Abort();
+            _webRequest = null;
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Localization Browser", EditorStyles.boldLabel);
+            _sourceSpreadsheetID = EditorGUILayout.TextField("Spreadsheet ID", _sourceSpreadsheetID);
+
+            EditorGUILayout.HelpBox($"Find the ID of a spreadsheet with this format: {_sourceSpreadsheetURLOriginalFormat}", MessageType.Info);
+
+            EditorGUILayout.Space();
+
+            GUILayout.Label("Data Operations (Requires Internet)", EditorStyles.boldLabel);
+            using (var _ = new GUILayout.HorizontalScope())
+            {
+                if (GUILayout.Button("Download", GUILayout.Height(40)))
+                {
+                    EditorPrefs.SetString(_sourceSpreadsheetIDKey + Application.productName, _sourceSpreadsheetID);
+                    StartDownloadingData(_sourceSpreadsheetID);
+                }
+
+                var url = _sourceSpreadsheetURLOriginalFormat.Replace(_IDToken, _sourceSpreadsheetID);
+                if (GUILayout.Button(new GUIContent("View on Google Sheets with Internet Browser...", url), GUILayout.Height(40)))
+                {
+                    FoxyLogger.Log($"Opening URL: {url}");
+                    Application.OpenURL(url);
+                }
+            }
+
+            GUILayoutExtensions.HorizontalLine();
+
+            if (IsDownloading)
+            {
+                EditorGUILayout.HelpBox($"Downloading...", MessageType.Info);
+            }
+            else if (Localization.IsInitialized)
+            {
+                DrawLanguageSelector();
+                EditorGUILayout.Space();
+                DrawLanguageTable();
+            }
+            else
+            {
+                EditorGUILayout.HelpBox($"Localization not correctly initialized. Please check for missing language data.", MessageType.Warning);
+            }
+        }
+
+        private void DrawLanguageSelector()
+        {
+            GUILayout.Label("Preview Languages", EditorStyles.boldLabel);
+            for (int i = 0; i < Localization.NumberOfLanguages; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    GUILayout.BeginHorizontal();
+                }
+                var buttonLabel = Localization.LanguageKeyName.GetLocalizationOverride(i);
+                var isVisible = i.IsInRange(VisibleLanguages) ? VisibleLanguages[i] : false;
+                if (GUILayoutExtensions.ColoredButton(buttonLabel, isVisible ? Color.yellow : Color.white, GUILayout.Width(InspectorWidth / 5)))
+                {
+                    VisibleLanguages[i] = !VisibleLanguages[i];
+                }
+                if (i % 5 == 4 || i == Localization.NumberOfLanguages - 1)
+                {
+                    GUILayout.EndHorizontal();
+                }
+            }
+        }
+
+        private void DrawLanguageTable()
+        {
+            var labelWidth = InspectorWidth / VisibleLanguageCountPlusOne - VisibleLanguageCountPlusOne * 0.25f;
+            GUILayout.Label("Localization Data", EditorStyles.boldLabel);
+
+            _searchKey = EditorGUILayoutExtensions.SearchText("Search Key", _searchKey);
+            var cacheSearchText = _searchKey.ToLower();
+
+            EditorGUILayout.Space();
+
+            using (var _ = new GUILayout.HorizontalScope())
+            {
+                GUILayout.Label(Localization.LanguageKeyName, EditorStyles.boldLabel, GUILayout.Width(labelWidth));
+                for (int i = 0; i < Localization.NumberOfLanguages; i++)
+                {
+                    if (i.IsInRange(VisibleLanguages) && VisibleLanguages[i])
+                    {
+                        GUILayout.Label(Localization.LanguageKeyName.GetLocalizationOverride(i), GUILayout.Width(labelWidth));
+                    }
+                }
+            }
+
+            _scroll = GUILayout.BeginScrollView(_scroll);
+            for (int i = 0; i < Localization.Keys.Count; i++)
+            {
+                var key = Localization.Keys[i];
+                if (key.ToLower().Contains(cacheSearchText))
+                {
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label(key, GUILayout.Width(labelWidth - 35));
+                    if (GUILayout.Button(EditorIcons.DuplicateIcon, GUILayout.Width(30)))
+                    {
+                        EditorGUIUtility.systemCopyBuffer = key;
+                        FoxyLogger.Log($"Key copied to system buffer: {key}");
+                    }
+                    GUI.enabled = false;
+                    for (int j = 0; j < Localization.NumberOfLanguages; j++)
+                    {
+                        if (j.IsInRange(VisibleLanguages) && VisibleLanguages[j])
+                        {
+                            EditorGUILayout.TextField(key.GetLocalizationOverride(j), GUILayout.Width(labelWidth));
+                        }
+                    }
+                    GUI.enabled = true;
+                    GUILayout.EndHorizontal();
+                }
+            }
+            GUILayout.EndScrollView();
+        }
+
+        #region Functions for downloading and parsing JSON text from Google Sheets
+        private void StartDownloadingData(string ID)
+        {
+            _webRequest = UnityWebRequest.Get(_sourceSpreadsheetURLJsonFormat.Replace(_IDToken, ID));
+            _webRequest.SendWebRequest();
+            EditorApplication.update += DownloadDataInProgress;
+        }
+
+        private void DownloadDataInProgress()
+        {
+            if (_webRequest == null || !_webRequest.isDone || _webRequest.result == UnityWebRequest.Result.InProgress)
+            {
+                return;
+            }
+
+            if (_webRequest.result == UnityWebRequest.Result.Success)
+            {
+                _downloadedJSONText = FetchTextInParentheses(_webRequest.downloadHandler.text);
+                if (TryParseJSONTextToLanguageAndKeyData(_downloadedJSONText, out _languageAndKeyData))
+                {
+                    WriteLanguageAndKeyDataToXMLFiles(_languageAndKeyData);
+                    Localization.LoadLanguageXMLAssets();
+                }
+                else
+                {
+                    FoxyLogger.LogError("Downloaded text json file contains invalid format. Cannot create language and key data.");
+                }
+            }
+            else
+            {
+                FoxyLogger.LogError($"Download failed. Error message: {_webRequest.error}");
+            }
+
+            EditorApplication.update -= DownloadDataInProgress;
+        }
+
+        private string FetchTextInParentheses(string source)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                return string.Empty;
+            }
+
+            var firstOpenedParenthesisIndex = source.IndexOf('(');
+            var lastClosedParenthesisIndex = source.LastIndexOf(')');
+
+            // If any parenthesis cannot be found, or if the opened parenthesis appears after the closed parenthesis: return empty.
+            if (firstOpenedParenthesisIndex < 0 || lastClosedParenthesisIndex < 0 || firstOpenedParenthesisIndex > lastClosedParenthesisIndex)
+            {
+                return string.Empty;
+            }
+
+            return source.Substring(firstOpenedParenthesisIndex + 1, lastClosedParenthesisIndex - firstOpenedParenthesisIndex - 1);
+        }
+
+        private bool TryParseJSONTextToLanguageAndKeyData(string originalText, out List<List<string>> languageAndKeyData)
+        {
+            languageAndKeyData = null;
+            var jObject = (JObject)JsonConvert.DeserializeObject(originalText);
+
+            if (!jObject.ContainsKey("table"))
+            {
+                LogMissingKeyFromDownloadedText("table");
+                return false;
+            }
+            var table = (JObject)jObject["table"];
+
+            if (!table.ContainsKey("rows"))
+            {
+                LogMissingKeyFromDownloadedText("rows");
+                return false;
+            }
+            var allRows = (JArray)table["rows"];
+
+            // Used to count the number of cells (i.e. number of languages) in the 0th row. After that, this is set to true.
+            bool languageCountSet = false;
+            int languageCount = 0;
+
+            // Temporary dictionary that will be cast to a nested list eventually.
+            List<string>[] tempDictionary = null;
+
+            foreach (var uncastRow in allRows)
+            {
+                if (uncastRow is not JObject row)
+                {
+                    FoxyLogger.LogError("Downloaded text parsing error. A null row exists.");
+                    return false;
+                }
+
+                if (!row.ContainsKey("c"))
+                {
+                    LogMissingKeyFromDownloadedText("c");
+                    return false;
+                }
+
+                int currentLanguageIndex = 0;
+                var cellsOnSingleRow = (JArray)row["c"];
+                foreach (var uncastCell in cellsOnSingleRow)
+                {
+                    if (uncastCell is JObject cell && cell.ContainsKey("v"))
+                    {
+                        if (languageCountSet && currentLanguageIndex.IsInRange(tempDictionary))
+                        {
+                            tempDictionary[currentLanguageIndex].Add(cell["v"].ToString());
+                        }
+                        else if (!languageCountSet && !string.IsNullOrWhiteSpace(cell["v"].ToString()))
+                        {
+                            languageCount++;
+                        }
+                    }
+                    else
+                    {
+                        if (languageCountSet && currentLanguageIndex.IsInRange(tempDictionary))
+                        {
+                            // Add an empty string if a valid value not exists, in this case uncastCell is null.
+                            tempDictionary[currentLanguageIndex].Add(string.Empty);
+                        }
+                    }
+                    currentLanguageIndex++;
+                }
+
+                // Set up the temporary dictionary after counting the 0th row.
+                if (!languageCountSet)
+                {
+                    languageCountSet = true;
+                    tempDictionary = new List<string>[languageCount];
+                    for (int x = 0; x < tempDictionary.Length; x++)
+                    {
+                        tempDictionary[x] = new List<string>();
+                    }
+                }
+            }
+
+            // Cast language data.
+            languageAndKeyData = tempDictionary.Cast<List<string>>().ToList();
+
+            return true;
+        }
+        #endregion
+
+        private void WriteLanguageAndKeyDataToXMLFiles(List<List<string>> languageAndKeyData)
+        {
+            if (languageAndKeyData.IsNullOrEmpty() || languageAndKeyData.Count <= 1)
+            {
+                return;
+            }
+
+            if (!Directory.Exists(_assetCreationFolder))
+            {
+                Directory.CreateDirectory(_assetCreationFolder);
+            }
+
+            // The 0th list is the key
+            var keys = languageAndKeyData[0];
+
+            // Actual language keys start from the 1st list
+            for (int i = 1; i < languageAndKeyData.Count; i++)
+            {
+                var content = new List<string> { "<?xml version=\"1.0\" encoding=\"UTF - 8\" standalone=\"yes\"?>", "<data>" };
+                var languageData = languageAndKeyData[i];
+                var languageName = languageData[0];
+                for (int j = 0; j < languageData.Count; j++)
+                {
+                    content.Add($"    <i k=\"{keys[j].ToXMLAttribute()}\" v=\"{languageData[j].ToXMLAttribute()}\"/>");
+                }
+                content.Add("</data>");
+                File.WriteAllLines(Path.Combine(_assetCreationFolder, $"{i}_{languageName}.xml"), content);
+            }
+            FoxyLogger.Log($"Successfully created {languageAndKeyData.Count - 1} language files.");
+            AssetDatabase.Refresh();
+        }
+
+        private void LogMissingKeyFromDownloadedText(string keyName)
+        {
+            FoxyLogger.LogError($"Downloaded text is missing a requied key: {keyName}.");
+        }
+    }
+}
---
+++ Scripts/Runtime/Localization/Localization.cs
@@ -0,0 +1,235 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using UnityEngine;
+
+namespace UrbanFox
+{
+    public static class Localization
+    {
+        public const string LanguageXMLAssetResourceFolder = "Languages";
+
+        private static Dictionary<string, string>[] _fullLocalizationData = null;
+        private static readonly List<string> _missingKeys = new List<string>();
+
+        public static Dictionary<string, string> CurrentLanguageData => CurrentLanguageIndex.IsInRange(_fullLocalizationData) ? _fullLocalizationData[CurrentLanguageIndex] : null;
+        public static int NumberOfLanguages => _fullLocalizationData.IsNullOrEmpty() ? 0 : _fullLocalizationData.Count();
+        public static List<string> Keys { get; private set; }
+        public static int CurrentLanguageIndex { get; private set; }
+        public static string LanguageKeyName { get; private set; }
+        public static bool IsInitialized { get; private set; }
+
+        static Localization()
+        {
+            LoadLanguageXMLAssets();
+        }
+
+        public static bool TryGetLocalization(this string key, out string value)
+        {
+            if (CurrentLanguageData != null)
+            {
+                if (CurrentLanguageData.ContainsKey(key))
+                {
+                    value = CurrentLanguageData[key];
+                    return true;
+                }
+            }
+            _missingKeys.Add(key);
+            value = $"!!{key}";
+            return false;
+        }
+
+        public static bool TryGetLocalizationOverride(this string key, int overrideLanguageIndex, out string value)
+        {
+            if (overrideLanguageIndex.IsInRange(_fullLocalizationData))
+            {
+                var overrideLanguageData = _fullLocalizationData[overrideLanguageIndex];
+                if (overrideLanguageData != null && overrideLanguageData.ContainsKey(key))
+                {
+                    value = overrideLanguageData[key];
+                    return true;
+                }
+            }
+            _missingKeys.Add(key);
+            value = $"!!{key}";
+            return false;
+        }
+
+        public static bool TryGetLocalizationOverride(this string key, string overrideLanguageName, out string value)
+        {
+            if (!_fullLocalizationData.IsNullOrEmpty())
+            {
+                for (int i = 0; i < _fullLocalizationData.Length; i++)
+                {
+                    var data = _fullLocalizationData[i];
+                    if (data != null && data.ContainsKey(LanguageKeyName) && data[LanguageKeyName] == overrideLanguageName)
+                    {
+                        value = data[key];
+                        return true;
+                    }
+                }
+            }
+            _missingKeys.Add(key);
+            value = $"!!{key}";
+            return false;
+        }
+
+        public static string GetLocalization(this string key)
+        {
+            key.TryGetLocalization(out var value);
+            return value;
+        }
+
+        public static string GetLocalizationOverride(this string key, int overrideLanguageIndex)
+        {
+            key.TryGetLocalizationOverride(overrideLanguageIndex, out var value);
+            return value;
+        }
+
+        public static string GetLocalizationOverride(this string key, string overrideLanguageName)
+        {
+            key.TryGetLocalizationOverride(overrideLanguageName, out var value);
+            return value;
+        }
+
+        public static string GetLanguageCodeNameByLanguageIndex(int languageIndex)
+        {
+            return languageIndex.IsInRange(_fullLocalizationData) ? _fullLocalizationData[languageIndex].First().Value : string.Empty;
+        }
+
+        public static void SetLanguage(int languageIndex)
+        {
+            if (languageIndex.IsInRange(_fullLocalizationData))
+            {
+                CurrentLanguageIndex = languageIndex;
+            }
+        }
+
+        public static void SetLanguage(string languageName)
+        {
+            if (!_fullLocalizationData.IsNullOrEmpty())
+            {
+                for (int i = 0; i < _fullLocalizationData.Length; i++)
+                {
+                    var data = _fullLocalizationData[i];
+                    if (data != null && data.ContainsKey(LanguageKeyName) && data[LanguageKeyName] == languageName)
+                    {
+                        SetLanguage(i);
+                        return;
+                    }
+                }
+            }
+        }
+
+        public static void ShiftLanguageRight()
+        {
+            if (!_fullLocalizationData.IsNullOrEmpty())
+            {
+                var nextIndex = CurrentLanguageIndex + 1;
+                if (nextIndex >= _fullLocalizationData.Length)
+                {
+                    nextIndex = 0;
+                }
+                SetLanguage(nextIndex);
+            }
+        }
+
+        public static void ShiftLanguageLeft()
+        {
+            if (!_fullLocalizationData.IsNullOrEmpty())
+            {
+                var nextIndex = CurrentLanguageIndex - 1;
+                if (nextIndex < 0)
+                {
+                    nextIndex = _fullLocalizationData.Length - 1;
+                }
+                SetLanguage(nextIndex);
+            }
+        }
+
+        public static void PrintMissingKeys()
+        {
+            if (!_missingKeys.IsNullOrEmpty())
+            {
+                FoxyLogger.LogError($"{_missingKeys.Count} missing keys found:\n - {string.Join("\n - ", _missingKeys)}");
+            }
+        }
+
+        public static void LoadLanguageXMLAssets()
+        {
+            LanguageKeyName = null;
+            _fullLocalizationData = null;
+            _missingKeys?.Clear();
+            IsInitialized = false;
+
+            var languageAssets = Resources.LoadAll<TextAsset>(LanguageXMLAssetResourceFolder);
+
+            if (languageAssets.IsNullOrEmpty())
+            {
+                FoxyLogger.LogWarning($"Language XML files cannot be found. Please move or download language files in Resource's \"{LanguageXMLAssetResourceFolder}\" folder.");
+                return;
+            }
+
+            _fullLocalizationData = new Dictionary<string, string>[languageAssets.Length];
+            try
+            {
+                for (int i = 0; i < languageAssets.Length; i++)
+                {
+                    var xmlDocument = new XmlDocument();
+                    try
+                    {
+                        xmlDocument.LoadXml(languageAssets[i].text);
+
+                        var root = xmlDocument.DocumentElement;
+                        var nodeList = root.SelectNodes("descendant::*");
+                        var thisDictionary = new Dictionary<string, string>();
+
+                        for (int j = 0; j < nodeList.Count; j++)
+                        {
+                            // Skip blank keys:
+                            if (string.IsNullOrWhiteSpace(nodeList[j].Attributes["k"].Value))
+                            {
+                                continue;
+                            }
+
+                            // Skip duplicated keys:
+                            if (thisDictionary.ContainsKey(nodeList[j].Attributes["k"].Value))
+                            {
+                                FoxyLogger.LogWarning($"Duplicated key exists: {nodeList[j].Attributes["k"]}");
+                                continue;
+                            }
+
+                            thisDictionary.Add(nodeList[j].Attributes["k"].Value.FromXMLAttribute(), nodeList[j].Attributes["v"].Value.FromXMLAttribute());
+
+                            // The key name is usually the 0th cell content:
+                            if (i == 0 && j == 0)
+                            {
+                                LanguageKeyName = nodeList[j].Attributes["k"].Value;
+                            }
+                        }
+
+                        if (i == 0)
+                        {
+                            Keys = thisDictionary.Keys.ToList();
+                        }
+
+                        _fullLocalizationData[i] = thisDictionary;
+                    }
+                    catch (Exception exception)
+                    {
+                        FoxyLogger.LogError($"Cannot load XML file: {languageAssets[i].name}.");
+                        FoxyLogger.LogException(exception);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                FoxyLogger.LogError($"Error exists when fetching language text documents.");
+                FoxyLogger.LogException(exception);
+            }
+
+            IsInitialized = true;
+        }
+    }
+}
