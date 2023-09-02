753dbcfafd73414dc0dd4f66e793102697481108
huchi57 <huchi.hello@gmail.com>
2023-09-02 18:34:49 +0900

    Print some temp debugs

--- Scripts/Runtime/Components/RuntimeManager.cs
+++ Scripts/Runtime/Components/RuntimeManager.cs
@@ -14,24 +14,30 @@ namespace UrbanFox
         {
             if (m_isApplicationQuitting)
             {
+                Debug.Log(2);
                 FoxyLogger.LogWarning($"An instance of {typeof(T)} will not be returned because the application is quitting.");
                 return null;
             }
             if (m_instance == null)
             {
+                Debug.Log(3);
                 m_instance = FindObjectOfType<T>();
                 if (m_instance == null)
                 {
+                    Debug.Log(4);
                     m_instance = new GameObject($"[{typeof(T).Name}]").AddComponent<T>();
                     FoxyLogger.Log($"An instance of {typeof(T)} has been automatically created because an instance could not be found.");
                 }
+                Debug.Log(5);
                 DontDestroyOnLoad(m_instance.transform.root);
             }
+            Debug.Log(6);
             return m_instance;
         }
 
         public virtual void OnAwake()
         {
+            Debug.Log(1);
             GetInstance();
         }
 
