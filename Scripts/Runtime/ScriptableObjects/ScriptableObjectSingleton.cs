f34a5637a8cc3d242dee55a1a7a2508b05cad38f
huchi57 <huchi.hello@gmail.com>
2023-09-14 21:24:06 +0900

    Update ScriptableObjectSingleton.cs

--- Scripts/Runtime/ScriptableObjects/ScriptableObjectSingleton.cs
+++ Scripts/Runtime/ScriptableObjects/ScriptableObjectSingleton.cs
@@ -4,23 +4,23 @@ namespace UrbanFox
 {
     public class ScriptableObjectSingleton<T> : ScriptableObject where T : ScriptableObject
     {
-        private static ScriptableObjectSingleton<T> m_instance;
+        private static T m_instance;
 
-        public static ScriptableObjectSingleton<T> Instance
+        public static T Instance
         {
             get
             {
                 if (m_instance == null)
                 {
-                    m_instance = Resources.Load<ScriptableObjectSingleton<T>>(string.Empty);
+                    m_instance = Resources.Load<T>(string.Empty);
                     if (m_instance == null)
                     {
-                        m_instance = CreateInstance<ScriptableObjectSingleton<T>>();
-                        FoxyLogger.Log($"A new instance of {typeof(ScriptableObjectSingleton<T>).Name} has been created in runtime. A runtime-generated instance will only be saved as an asset in Editor.");
+                        m_instance = CreateInstance<T>();
+                        FoxyLogger.Log($"A new instance of {typeof(T).Name} has been created in runtime. A runtime-generated instance will only be saved as an asset in Editor.");
 #if UNITY_EDITOR
-                        var newAssetPath = UnityEditor.AssetDatabase.GenerateUniqueAssetPath($"Assets/Resources/{typeof(ScriptableObjectSingleton<T>).Name}.asset");
+                        var newAssetPath = UnityEditor.AssetDatabase.GenerateUniqueAssetPath($"Assets/Resources/{typeof(T).Name}.asset");
                         UnityEditor.AssetDatabase.CreateAsset(m_instance, newAssetPath);
-                        FoxyLogger.Log($"A new instance of {typeof(ScriptableObjectSingleton<T>).Name} has been saved to the asset at {newAssetPath}.");
+                        FoxyLogger.Log($"A new instance of {typeof(T).Name} has been saved to the asset at {newAssetPath}.");
 #endif
                     }
                 }
