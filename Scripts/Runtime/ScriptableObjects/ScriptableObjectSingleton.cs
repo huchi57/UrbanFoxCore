806e46a0e4dd0cd963a6eb3736308096da54d87f
huchi57 <huchi.hello@gmail.com>
2023-09-14 21:34:10 +0900

    Update ScriptableObjectSingleton.cs

--- Scripts/Runtime/ScriptableObjects/ScriptableObjectSingleton.cs
+++ Scripts/Runtime/ScriptableObjects/ScriptableObjectSingleton.cs
@@ -12,17 +12,27 @@ namespace UrbanFox
             {
                 if (m_instance == null)
                 {
-                    m_instance = Resources.Load<T>(string.Empty);
-                    if (m_instance == null)
+                    var candidates = Resources.LoadAll<T>(string.Empty);
+                    if (!candidates.IsNullOrEmpty())
                     {
-                        m_instance = CreateInstance<T>();
-                        FoxyLogger.Log($"A new instance of {typeof(T).Name} has been created in runtime. A runtime-generated instance will only be saved as an asset in Editor.");
-#if UNITY_EDITOR
-                        var newAssetPath = UnityEditor.AssetDatabase.GenerateUniqueAssetPath($"Assets/Resources/{typeof(T).Name}.asset");
-                        UnityEditor.AssetDatabase.CreateAsset(m_instance, newAssetPath);
-                        FoxyLogger.Log($"A new instance of {typeof(T).Name} has been saved to the asset at {newAssetPath}.");
-#endif
+                        if (candidates.Length > 1)
+                        {
+                            FoxyLogger.Log($"2 or more {typeof(T).Name} instances found. Only the first found instance will be referenced. Consider removing other files of the same type.");
+                        }
+                        m_instance = candidates[0];
+                        if (m_instance != null)
+                        {
+                            return m_instance;
+                        }
                     }
+
+                    m_instance = CreateInstance<T>();
+                    FoxyLogger.Log($"A new instance of {typeof(T).Name} has been created in runtime. A runtime-generated instance will only be saved as an asset in Editor.");
+#if UNITY_EDITOR
+                    var newAssetPath = UnityEditor.AssetDatabase.GenerateUniqueAssetPath($"Assets/Resources/{typeof(T).Name}.asset");
+                    UnityEditor.AssetDatabase.CreateAsset(m_instance, newAssetPath);
+                    FoxyLogger.Log($"A new instance of {typeof(T).Name} has been saved to the asset at {newAssetPath}.");
+#endif
                 }
                 return m_instance;
             }
