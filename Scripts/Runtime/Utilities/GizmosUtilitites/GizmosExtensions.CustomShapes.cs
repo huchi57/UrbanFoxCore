adaf5313c8eae7fefbb37ba37e8718f18882ca77
huchi57 <huchi.hello@gmail.com>
2023-09-30 18:30:15 +0900

    Feat: Added wire cylinder and wire cone

--- Scripts/Runtime/Utilities/GizmosUtilitites/GizmosExtensions.CustomShapes.cs
+++ Scripts/Runtime/Utilities/GizmosUtilitites/GizmosExtensions.CustomShapes.cs
@@ -23,5 +23,51 @@ namespace UrbanFox
         {
             DrawWireDisc(center, normal, radius, Gizmos.color);
         }
+
+        public static void DrawWireCylinder(Vector3 surfaceACenter, Vector3 surfaceBCenter, float radius, Color color)
+        {
+            using (new GizmosScope(color))
+            {
+                var direction = (surfaceBCenter - surfaceACenter).normalized;
+                DrawWireDisc(surfaceACenter, direction, radius);
+                DrawWireDisc(surfaceBCenter, direction, radius);
+
+                var perpendicularVector = direction.GetPerpendicularVector();
+                Gizmos.DrawLine(surfaceACenter + radius * perpendicularVector, surfaceBCenter + radius * perpendicularVector);
+                Gizmos.DrawLine(surfaceACenter - radius * perpendicularVector, surfaceBCenter - radius * perpendicularVector);
+
+                perpendicularVector = Quaternion.AngleAxis(90, direction) * perpendicularVector;
+                Gizmos.DrawLine(surfaceACenter + radius * perpendicularVector, surfaceBCenter + radius * perpendicularVector);
+                Gizmos.DrawLine(surfaceACenter - radius * perpendicularVector, surfaceBCenter - radius * perpendicularVector);
+            }
+        }
+
+        public static void DrawWireCylinder(Vector3 surfaceACenter, Vector3 surfaceBCenter, float radius)
+        {
+            DrawWireCylinder(surfaceACenter, surfaceBCenter, radius, Gizmos.color);
+        }
+
+        public static void DrawWireCone(Vector3 baseCenter, float baseRadius, Vector3 coneDirection, float coneHeight, Color color)
+        {
+            using (new GizmosScope(color))
+            {
+                var apex = baseCenter + coneHeight * coneDirection;
+                DrawWireDisc(baseCenter, coneDirection, baseRadius);
+                Gizmos.DrawLine(baseCenter, apex);
+
+                var perpendicularVector = coneDirection.GetPerpendicularVector();
+                Gizmos.DrawLine(apex, baseCenter + baseRadius * perpendicularVector);
+                Gizmos.DrawLine(apex, baseCenter - baseRadius * perpendicularVector);
+
+                perpendicularVector = Quaternion.AngleAxis(90, coneDirection) * perpendicularVector;
+                Gizmos.DrawLine(apex, baseCenter + baseRadius * perpendicularVector);
+                Gizmos.DrawLine(apex, baseCenter - baseRadius * perpendicularVector);
+            }
+        }
+
+        public static void DrawWireCone(Vector3 baseCenter, float baseRadius, Vector3 coneDirection, float coneHeight)
+        {
+            DrawWireCone(baseCenter, baseRadius, coneDirection, coneHeight, Gizmos.color);
+        }
     }
 }
