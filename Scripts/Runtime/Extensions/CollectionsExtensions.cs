8b7dd4e2f2accd09dd0981e190a87897ef82432c
huchi57 <huchi.hello@gmail.com>
2024-02-07 15:03:40 +0900

    Update CollectionsExtensions.cs

--- Scripts/Runtime/Extensions/CollectionsExtensions.cs
+++ Scripts/Runtime/Extensions/CollectionsExtensions.cs
@@ -1,29 +1,30 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace UrbanFox
 {
     public static class CollectionsExtensions
     {
-        public static bool IsNullOrEmpty<T>(this ICollection<T> collection)
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
         {
             if (collection == null)
             {
                 return true;
             }
-            return collection.Count <= 0;
+            return collection.Count() <= 0;
         }
 
-        public static bool IsInRange<T>(this int index, ICollection<T> collection)
+        public static bool IsInRange<T>(this int index, IEnumerable<T> collection)
         {
             if (collection.IsNullOrEmpty())
             {
                 return false;
             }
-            return index.IsInRange(0, collection.Count - 1);
+            return index.IsInRange(0, collection.Count() - 1);
         }
 
-        public static bool IsIndexInRange<T>(this ICollection<T> collection, int index)
+        public static bool IsIndexInRange<T>(this IEnumerable<T> collection, int index)
         {
             return index.IsInRange(collection);
         }
