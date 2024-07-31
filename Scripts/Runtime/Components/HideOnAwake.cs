074868ec280be03aff5a247ca503f8e4c4d6be01
huchi57 <huchi.hello@gmail.com>
2024-07-31 14:14:50 +0900

    Feat: Hide on awake

---
+++ Scripts/Runtime/Components/HideOnAwake.cs
@@ -0,0 +1,63 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UrbanFox
+{
+    public class HideOnAwake : MonoBehaviour
+    {
+        [Serializable]
+        public enum OnAwakeAction
+        {
+            DoNothing,
+            SetGameObjectInactive,
+            HideGraphic,
+            HideMeshRenderer,
+            DestroyGameObject
+        }
+
+        [SerializeField]
+        private OnAwakeAction m_onAwake = OnAwakeAction.SetGameObjectInactive;
+
+        [SerializeField, ShowIf(nameof(m_onAwake), OnAwakeAction.HideGraphic), NonEditable]
+        private Graphic m_graphic;
+
+        [SerializeField, ShowIf(nameof(m_onAwake), OnAwakeAction.HideMeshRenderer), NonEditable]
+        private MeshRenderer m_meshRenderer;
+
+        private void OnValidate()
+        {
+            m_graphic = GetComponent<Graphic>();
+            m_meshRenderer = GetComponent<MeshRenderer>();
+        }
+
+        private void Awake()
+        {
+            switch (m_onAwake)
+            {
+                case OnAwakeAction.DoNothing:
+                    break;
+                case OnAwakeAction.SetGameObjectInactive:
+                    gameObject.SetActive(false);
+                    break;
+                case OnAwakeAction.HideGraphic:
+                    if (m_graphic || TryGetComponent(out m_graphic))
+                    {
+                        m_graphic.enabled = false;
+                    }
+                    break;
+                case OnAwakeAction.HideMeshRenderer:
+                    if (m_meshRenderer || TryGetComponent(out m_meshRenderer))
+                    {
+                        m_meshRenderer.enabled = false;
+                    }
+                    break;
+                case OnAwakeAction.DestroyGameObject:
+                    Destroy(gameObject);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
